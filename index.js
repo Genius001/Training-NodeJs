8333d6bc11125da2ca83c145725a91c7afdc21ed
steven aditya <stv_Aditya@yahoo.com>
2024-10-17 16:19:07 +0700

    makan

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 const envPath = process.env.NODE_ENV === 'development' ?
-  '.env' : `.env.${process.env.NODE_ENV}`
-console.log(envPath)
-require('dotenv').config({ path: envPath })
+  '.env' : `.env.${process.env.NODE_ENV || 'development'}`;
+console.log('Using environment file:', envPath);
+
+require('dotenv').config({ path: envPath });
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require('cors');
-const routes = require('./src/routes')
+const routes = require('./src/routes');
 const errorHandler = require("./src/middlewares/errorHandler");
 
+// Log loaded environment variables
+console.log('Loaded environment variables:', process.env);
 
 const { PORT = 3000 } = process.env;
 
-// const server = http.createServer(app);
-
-// untuk meregistrasi global variable untuk error handling
+// Register global variable for error handling
 require("./src/helpers/errors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use("/public", express.static(path.resolve(__dirname, "public")));
 
 app.get('/', async (req, res) => {
-  res.status(200).send('Car Rental API')
-})
+  res.status(200).send('Car Rental API');
+});
 
-app.use('/api/v1', routes)
+app.use('/api/v1', routes);
 
-//application level middleware untuk error handling
-app.use(errorHandler)
+// Application level middleware for error handling
+app.use(errorHandler);
 
 app.use((req, res, next) => {
   next(new NotFoundError(null, "Sorry, page not found!"));
-})
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
